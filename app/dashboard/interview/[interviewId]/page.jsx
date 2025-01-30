89dbde4986b719d18f02cb1ca9bd418a2203cd0f
Nayan Das <nayan010das@gmail.com>
2025-01-30 19:24:22 +0530

    Now We can see our given info in interview page

--- app/dashboard/interview/[interviewId]/page.jsx
+++ app/dashboard/interview/[interviewId]/page.jsx
@@ -21,27 +21,40 @@ function Interview({params}) {
 
     const GetInterviewDetails= async()=>{
         const res = await db.select().from(MockInterview).where(eq(MockInterview.mockId,interviewId));
-        //console.log("interview",res);
+        //console.log("interview",res[0].jobPosition);
         setInterviewData(res[0]);
     }
+
+    function capitalizeFirstLetter(str) {
+        if (!str) return str; // Handle empty strings
+        return str.charAt(0).toUpperCase() + str.slice(1);
+      }
 
   return (
     <div className='my-10 flex justify-center flex-col items-center'>
         <h2 className='font-bold text-2xl'>Let's Get Started</h2>
 
-        <div>
+        <div className='flex flex-col items-center'>
             {
                 webcamEnable ? 
                 <Webcam
                 onUserMedia={()=>setWebcamEnable(true)}
                 onUserMediaError={()=>setWebcamEnable(false)}
+                mirrored={true}
                 style={{
                     height:300,
                     width:300
+                    
                 }}/> : 
                 <WebcamIcon className='h-72 w-full my-7 p-10 bg-secondary rounded-lg border'/>
             }
-            <Button className="bg-primary text-white" onClick={()=>setWebcamEnable(true)}>Start Webcam</Button>
+            <Button className="bg-primary text-white w-full" onClick={()=>setWebcamEnable(true)}>Start Webcam</Button>
+        </div>
+
+        <div className='flex flex-col my-5 items-center gap-2'>
+            <h2><strong>Job Position/Role: </strong>{capitalizeFirstLetter(interviewData?.jobPosition)}</h2>
+            <h2><strong>Job Description: </strong>{capitalizeFirstLetter(interviewData?.jobDescription)}</h2>
+            <h2><strong>Years of Experience: </strong>{capitalizeFirstLetter(interviewData?.jobExperience)}</h2>
         </div>
     </div>
   )
